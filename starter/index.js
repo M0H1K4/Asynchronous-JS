abdf45bef151a0789a237d296f665a035cdd15c9
M0H1K4N <nejer911@gmail.com>
2023-11-24 16:46:32 +0400

    Ad

--- starter/index.js
+++ starter/index.js
@@ -1,23 +1,38 @@
 const fs = require("fs");
+const { resolve } = require("path");
 const superagent = require("superagent");
 
 /* 
 ფოტოს API წამოვიღეთ საიტიდან და დააგენერირა~
 ფოტო იმის მიხედვით თუ რა ჯიშიც მივუთთეთ txt ფაილში
+we used there callBack() => ||| and also "promise"
 */
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject("I could not find that file bby ❤️");
+      resolve(data);
+    });
+  });
+};
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+readFilePro(`${__dirname}/dog.txt`).then((data) => {
   console.log(`Breed: ${data}`);
 
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
-        if (err) return console.log(err.message);
-        console.log(res.body.message);
-      
+    .then((res) => {
+      console.log(res.body.message);
+
       fs.writeFile("dog-img-txt", res.body.message, (err) => {
         if (err) return console.log(err.message);
         console.log("Random dog image saved to file");
       });
+    })
+
+    // helps us to catch errors easyer
+    .catch((err) => {
+      console.log(err.message);
     });
 });
+
